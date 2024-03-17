ee8556effe6a24b5b6e584831e61e0e07d7d6fc4
virtualp1 <zininnikita309@gmail.com>
2024-03-18 04:13:18 +0900

    remove css config option

--- vite.config.ts
+++ vite.config.ts
@@ -6,13 +6,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  css: {
-    preprocessorOptions: {
-      less: {
-        javascriptEnabled: true,
-      },
-    },
-  },
   resolve: {
     alias: [
       {
