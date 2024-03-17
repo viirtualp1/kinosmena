2a517ab831b2fcb0c2b705fa5b501f3a527fb5e1
virtualp1 <zininnikita309@gmail.com>
2024-03-18 03:15:49 +0900

    remove ui button

--- src/components/MainPage/MainPage.tsx
+++ src/components/MainPage/MainPage.tsx
@@ -1,20 +1,7 @@
-import { KsButton } from '@/components/ui'
-import { useMainButton } from '@/hooks/useMainButton'
-
 export function MainPage() {
-  const { toggle } = useMainButton()
-
   return (
     <div className="container content">
       <h1>Kinosmena</h1>
-
-      <KsButton
-        text="Toggle main button"
-        theme="accent"
-        size="large"
-        block
-        onClick={toggle}
-      />
     </div>
   )
 }
