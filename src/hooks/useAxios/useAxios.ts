125dedd6b4ea6dba5096e395b563739dfb8796c5
virtualp1 <zininnikita309@gmail.com>
2024-03-29 05:42:19 +0900

    fixes

--- src/hooks/useAxios/useAxios.ts
+++ src/hooks/useAxios/useAxios.ts
@@ -3,7 +3,7 @@ import axios from 'axios'
 export default axios.create({
   baseURL: 'http://rbychin.ddns.net:6080/api/',
   params: {
-    // telegram user id
-    tid: 1,
+    // TODO: Получать ID из Telegram API
+    tid: import.meta.env.TEST_TID,
   },
 })
