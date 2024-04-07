84c08da14104cb90f510c8b87cdff04764f6b927
virtualp1 <zininnikita309@gmail.com>
2024-04-08 08:11:21 +0900

    new api

--- src/hooks/useAxios/useAxios.ts
+++ src/hooks/useAxios/useAxios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export default axios.create({
-  baseURL: 'http://rbychin.ddns.net:6080/api/',
+  baseURL: 'https://rbychin.ddns.net/kinosmena/api/',
   params: {
     // TODO: Получать ID из Telegram API
     tid: import.meta.env.TEST_TID,
